bb1acc976e1faec7f377173189c4031dd4f1732e
David Smith <DSmith@RealClearPolitics.com>
2020-03-01 17:07:30 -0500

    Update tag line.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="title">MyNutrition</h1>
-      <h2 class="subtitle">MyNutrition.com Web Application</h2>
+      <h1 class="title">MyNutrition.com</h1>
+      <h2 class="subtitle">Nutrition Reinvented.</h2>
     </div>
   </div>
 </template>
@@ -11,9 +11,9 @@
 import Logo from '~/components/Logo.vue'
 
 export default {
-    components: {
-        Logo
-    }
+  components: {
+    Logo
+  }
 }
 </script>
 
@@ -24,34 +24,33 @@ export default {
 }
 */
 .container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
 
 .title {
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
-        BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
-        sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
 }
 
 .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
 }
 
 .links {
-    padding-top: 15px;
+  padding-top: 15px;
 }
 </style>
