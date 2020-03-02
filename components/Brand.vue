ed7bb2ee5670211b322ff46a1821ba76a222b81e
David Smith <davidsmith@itsandbox.net>
2020-03-02 16:54:15 -0500

    Add Brand.vue component.

---
+++ components/Brand.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1
+      class="text-5xl text-center font-hairline text-gray-900 tracking-tighter"
+    >
+      <span class="text-blue-700">My</span>Nutrition.com
+    </h1>
+    <h2 class="text-2xl text-center font-light text-gray-600 tracking-widest">
+      nutrition
+      <span class="text-gray-800">reinvented</span>
+    </h2>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,14 @@
 <template>
   <div class="container m-auto">
-    <div>
-      <h1
-        class="text-5xl text-center font-hairline text-gray-900 tracking-tighter"
-      >
-        <span class="text-blue-700">My</span>Nutrition.com
-      </h1>
-      <h2 class="text-2xl text-center font-light text-gray-600 tracking-widest">
-        nutrition <span class="text-gray-800">reinvented</span>
-      </h2>
-      <!--<nuxt-link to="products">Products</nuxt-link>-->
-    </div>
+    <Brand></Brand>
   </div>
 </template>
 
 <script>
-export default {}
+import Brand from '~/components/Brand.vue'
+export default {
+  components: {
+    Brand
+  }
+}
 </script>
