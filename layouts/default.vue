68833c132be6b01b501ea43aa4dbf5c4665a8bbd
David Smith <davidsmith@itsandbox.net>
2020-03-02 16:07:09 -0500

    Update Title

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,12 @@
     <nuxt />
   </div>
 </template>
+<script>
+export default {
+  head: {
+    bodyAttrs: {
+      class: 'bg-gray-100'
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="container">
+  <div class="container m-auto">
     <div>
-      <h1 class="title">MyNutrition.com</h1>
-      <h2 class="subtitle">Nutrition Reinvented.</h2>
-      <nuxt-link to="products">Products</nuxt-link>
+      <h1
+        class="text-5xl text-center font-hairline text-gray-900 tracking-tighter"
+      >
+        <span class="text-blue-700">My</span>Nutrition.com
+      </h1>
+      <h2 class="text-2xl text-center font-light text-gray-600 tracking-widest">
+        nutrition <span class="text-gray-800">reinvented</span>
+      </h2>
+      <!--<nuxt-link to="products">Products</nuxt-link>-->
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
