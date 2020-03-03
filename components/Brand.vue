7771e3b2410fab4d6f878b8948ed80ea8281b3fb
David Smith <DSmith@RealClearPolitics.com>
2020-03-02 21:12:41 -0500

    Update logo

--- components/Brand.vue
+++ components/Brand.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-5xl text-center font-hairline text-gray-900 tracking-tight">
+    <h1 class="text-5xl text-center font-light text-gray-900 tracking-tight">
       <span class="text-blue-700">My</span>Nutrition
     </h1>
     <h2 class="text-2xl text-center font-light text-gray-600 tracking-wider">
