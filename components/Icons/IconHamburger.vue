171fc79d4accf14bcd38b567bc567176571ed192
David Smith <davidsmith@itsandbox.net>
2020-03-03 15:02:00 -0500

    Add Icon

--- components/Icons/IconHamburger.vue
+++ components/Icons/IconHamburger.vue
@@ -1,6 +1,6 @@
 <template
   ><div>
-    <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+    <svg class="h-8 w-8 fill-current" viewBox="0 0 24 24">
       <path
         fill-rule="evenodd"
         d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
--- components/Navbars/NavbarMain.vue
+++ components/Navbars/NavbarMain.vue
@@ -1,10 +1,10 @@
 <template>
   <header
-    class="bg-gray-900 flex items-center justify-between align-items-center py-1 px-1"
+    class="bg-gray-900 flex items-center justify-between align-items-center py-3 px-2"
   >
     <nuxt-link to="/" class="w-1/3"><TextLogo config="NavbarMain"/></nuxt-link>
     <div class="text-gray-100 w-1/3 text-center">
-      <nuxt-link to="products">Products</nuxt-link>
+      <nuxt-link to="products" class="text-xl">Products</nuxt-link>
     </div>
     <ButtonHamburger class="w-1/3 flex justify-end" />
   </header>
