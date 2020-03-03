e6bf1628e649fd42ec17349f0f7f79dfd2de03dd
David Smith <DSmith@RealClearPolitics.com>
2020-03-02 21:06:24 -0500

    WIP

--- components/Brand.vue
+++ components/Brand.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
-    <h1
-      class="text-5xl text-center font-hairline text-gray-900 tracking-tighter"
-    >
-      <span class="text-blue-700">My</span>Nutrition.com
+    <h1 class="text-5xl text-center font-hairline text-gray-900 tracking-tight">
+      <span class="text-blue-700">My</span>Nutrition
     </h1>
-    <h2 class="text-2xl text-center font-light text-gray-600 tracking-widest">
+    <h2 class="text-2xl text-center font-light text-gray-600 tracking-wider">
       nutrition
       <span class="text-gray-800">reinvented</span>
     </h2>
