7110a2e19dd5c3044e704ed0c291e1294d9ba14d
David Smith <davidsmith@itsandbox.net>
2020-03-05 14:55:30 -0500

    Update navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="sm:text-center p-6 sm:p-2 pt-20 banner bg-center bg-cover bg-no-repeat "
+    class="sm:text-center p-6 sm:p-2 pt-20 banner bg-center bg-cover bg-no-repeat h-screen"
   >
     <div class="inline-block my-20 sm:mt-10">
       <TextLogo class="bg-gray-200 p-4 pt-0 rounded-lg transform sm:scale-75" />
