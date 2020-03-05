fd3e6472ff81b3b5bed13dd22520c5846c5001e5
David Smith <DSmith@RealClearPolitics.com>
2020-03-04 20:59:27 -0500

    Update Logo

--- components/TextLogo/TextLogoTagline.vue
+++ components/TextLogo/TextLogoTagline.vue
@@ -2,7 +2,7 @@
   <h2 class="text-2xl text-blue-700 tracking-widest leading-none">
     pre-workout
     <span class="text-gray-600 font-light"
-      ><span class="text-gray-800">re</span>imagined</span
+      ><span class="text-gray-700">re</span>imagined</span
     >
   </h2></template
 >
