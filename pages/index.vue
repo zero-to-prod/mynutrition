effa4a4f92ef8fc7c0a08579cf8e44e8d63a9a49
David Smith <davidsmith@itsandbox.net>
2020-03-05 14:42:29 -0500

    Update navbar

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   <div
     class="sm:text-center p-6 sm:p-2 pt-20 banner bg-center bg-cover bg-no-repeat "
   >
-    <div class="inline-block my-20">
+    <div class="inline-block my-20 sm:mt-10">
       <TextLogo class="bg-gray-200 p-4 pt-0 rounded-lg transform sm:scale-75" />
       <button
-        class="bg-blue-700 m:scale-90 text-gray-100 p-4 shadow-md rounded-lg my-10 sm:my-2 text-xl hover:shadow-lg hover:bg-blue-600 transform hover:scale-105 transition duration-150 "
+        class="bg-blue-700 transform sm:scale-90 text-gray-100 p-4 shadow-md rounded-lg my-10 text-xl hover:shadow-lg hover:bg-blue-600 transform hover:scale-105 transition duration-150 "
       >
         <nuxt-link to="/order">Custom PRE-WORKOUT</nuxt-link>
       </button>
