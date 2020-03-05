8e822351e5f25f661926e937ea4670076fd88ca8
David Smith <davidsmith@itsandbox.net>
2020-03-05 13:15:16 -0500

    Update navbar

--- components/TextLogo/TextLogoFull.vue
+++ components/TextLogo/TextLogoFull.vue
@@ -5,14 +5,14 @@
   </div>
 </template>
 <script>
-import TextLogo from '~/components/TextLogo/TextLogo.vue'
-import Tagline from '~/components/TextLogo/TextLogoTagline.vue'
+import TextLogo from '~/components/TextLogo/TextLogo.vue';
+import Tagline from '~/components/TextLogo/TextLogoTagline.vue';
 export default {
   components: {
     TextLogo,
     Tagline
   },
   name: 'TextLogoFull'
-}
+};
 </script>
 <style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <div
-      class="sm:text-center left-1 relative p-6 pt-20 banner bg-local h-full"
-    >
-      <TextLogo />
+  <div
+    class="sm:text-center p-6 sm:p-2 pt-20 banner bg-center bg-cover bg-no-repeat "
+  >
+    <div class="inline-block sm:object-scale-down transform sm:scale-75">
+      <TextLogo class="bg-gray-200 p-4 rounded-lg" />
       <button
-        class="bg-blue-700 text-gray-100 p-4 shadow-md rounded-lg my-10 text-xl hover:shadow-lg hover:bg-blue-600 transform hover:scale-105 transition duration-150 "
+        class="transform sm:scale-90 bg-blue-700 text-gray-100 p-4 shadow-md rounded-lg my-10 text-xl hover:shadow-lg hover:bg-blue-600 transform hover:scale-105 transition duration-150 "
       >
         Customize my PRE-WORKOUT
       </button>
